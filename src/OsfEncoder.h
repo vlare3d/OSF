94702da14c9072328d49cbc55d4ff247bbd27fa2
vlare3d <96245862+vlare3d@users.noreply.github.com>
2022-08-16 09:53:56 +0800

    :zap: update osf format,add 11 parameters
    Number of reserved: This field has 20 bytes, 18 bytes are used, and these 18 bytes contain 11 parameters:

--- src/OsfEncoder.h
+++ src/OsfEncoder.h
@@ -4,48 +4,48 @@
 #include <vector>
 #include <mutex>
 
-struct IconImg 
+struct IconImg
 {
-	unsigned char* data;
-	unsigned int len;
+    unsigned char* data;
+    unsigned int len;
 };
 
 struct PointSet
 {
-	int gray;
-	int count;
-	PointSet(int g, int c) : gray(g), count(c) {}
+    int gray;
+    int count;
+    PointSet(int g, int c) : gray(g), count(c) {}
 };
 
 struct SliceData
 {
-	int layerId;
-	std::vector<PointSet> pointsWithSupport;
-	std::vector<PointSet> pointsSupport;
+    int layerId;
+    std::vector<PointSet> pointsWithSupport;
+    std::vector<PointSet> pointsSupport;
 };
 
 class osfEncoder
 {
 public:
-	using DlpData = std::vector<unsigned char>;
+    using DlpData = std::vector<unsigned char>;
 
-	void setConfig(OsfConfig config);
-	void addData(const DlpData& data, const unsigned int& id);
-	void addData(const DlpData &data, const DlpData &dataSupport, const unsigned int &id);
-	void addIconImg(const IconImg& img1, const IconImg &img2, const IconImg &img3, const IconImg &img4);
-	void saveOsfFile(const std::string& filename);
+    void setConfig(OsfConfig config);
+    void addData(const DlpData& data, const unsigned int& id);
+    void addData(const DlpData &data, const DlpData &dataSupport, const unsigned int &id);
+    void addIconImg(const IconImg& img1, const IconImg &img2, const IconImg &img3, const IconImg &img4);
+    void saveOsfFile(const std::string& filename);
 
 private:
-	enum class HeadType { WithSupport, SupportOnly };
-	void _3BOut(std::ofstream &out, int data);
-	void points2Stream(std::ofstream &out, const std::vector<PointSet> &htPointSets, HeadType headType);
-	void singleSet2Stream(std::ofstream &out, int gray, int count);
-	void raw2Points(const DlpData &data, std::vector<PointSet> &htPointSets);
+    enum class HeadType { WithSupport, SupportOnly };
+    void _3BOut(std::ofstream &out, int data);
+    void points2Stream(std::ofstream &out, const std::vector<PointSet> &htPointSets, HeadType headType);
+    void singleSet2Stream(std::ofstream &out, int gray, int count);
+    void raw2Points(const DlpData &data, std::vector<PointSet> &htPointSets);
 
-	const short _version = 0x0001;
-	IconImg _img1, _img2, _img3, _img4;
-	OsfConfig _config;
-	std::vector<SliceData> _SliceDataVec;
+    const short _version = 0x0004;
+    IconImg _img1, _img2, _img3, _img4;
+    OsfConfig _config;
+    std::vector<SliceData> _SliceDataVec;
 
 };
 
